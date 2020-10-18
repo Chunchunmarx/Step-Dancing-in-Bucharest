f0032931ee834ffb4f8ea41600968e33bbb9cb77
S-Buzatu <sorin_buzatu@yahoo.com>
2020-10-18 20:08:55 +0300

    Created basic map, basic player, basic movement.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public float speed;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2( moveHorizontal, moveVertical);
+        rb2d.velocity = movement * speed;
+        Debug.Log("Velocity: "+ rb2d.velocity);
+    }
+}
